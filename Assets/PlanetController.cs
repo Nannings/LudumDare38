b151f44ee49cca84dd41cb17575d121ed420531b
Nannings <mrnannings@gmail.com>
2017-04-23 21:43:19 +0200

    more updates;

--- Assets/PlanetController.cs
+++ Assets/PlanetController.cs
@@ -7,21 +7,15 @@ public class PlanetController : MonoBehaviour
     [SerializeField]
     private Sprite[] sprites;
 
-    private float scale;
-
 	// Use this for initialization
 	void Start ()
     {
         GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
-
-        scale = Random.Range(1f, 3f);
-        
-        transform.localScale = new Vector3(scale, scale, transform.localScale.z);
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
 	}
+
 }
---
+++ Assets/ScaleBreath.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleBreath : MonoBehaviour {
+
+    [SerializeField]
+    private float randomScale;
+    [SerializeField]
+    private float scaleRate = 0.1f;
+
+    private float scale;
+    private bool scaleToggle = false;
+
+    // Use this for initialization
+    void Start () {
+        scale = transform.localScale.x;
+        if (randomScale > 0) scale = Random.Range(1f, randomScale);
+
+        transform.localScale = new Vector3(scale, scale, transform.localScale.z);
+
+        StartCoroutine(CoScaleToggle());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (scaleToggle)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(scale + scaleRate, scale + scaleRate, transform.localScale.z), Time.deltaTime);
+        }
+        else
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(scale - scaleRate, scale - scaleRate, transform.localScale.z), Time.deltaTime);
+        }
+    }
+
+    IEnumerator CoScaleToggle()
+    {
+        yield return new WaitForSeconds(1);
+        scaleToggle = !scaleToggle;
+        StartCoroutine(CoScaleToggle());
+    }
+}
